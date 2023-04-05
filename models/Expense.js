5534c18f8049a85f66963393d053fb242603f8be
SaintOfMirren <drew.miller2015@gmail.com>
2023-04-04 19:47:30 -0700

    added Expense.js model, updated index and user models

---
+++ models/Expense.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Expense extends Model { }
+
+Expense.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        category_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        amount: {
+            type: DataTypes.DECIMAL(10, 2),
+            allowNull: false,
+        },
+        date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'Expense',
+    }
+);
+
+module.exports = Expense;
--- models/index.js
+++ models/index.js
@@ -1,13 +1,13 @@
 const User = require('./User');
-const Project = require('./Project');
+const Expense = require('./Expense');
 
-User.hasMany(Project, {
+User.hasMany(Expense, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
-Project.belongsTo(User, {
+Expense.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Project };
+module.exports = { User, Expense };
