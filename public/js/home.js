fa52adc5c9bbb8ce57e580838e872d39adb6f686
Bootcamp <bootcamp@Abduls-MacBook-Air.local>
2023-04-09 05:39:00 -0700

    Percentage sections and  calculate buttons added

---
+++ public/js/home.js
@@ -0,0 +1,26 @@
+// declare global variables
+// declare functions
+// setup event listeners
+
+
+
+// const plusBtns = document.querySelectorAll('.budget-section h3 button')
+// const sectionContentElements = document.querySelectorAll('.budget-section--content')
+// const calcBtn = document.getElementById('calcBtn')
+// const budgetForm = document.getElementById('budget')
+
+// plusBtns.forEach((btn, btnIndex) => {
+//     btn.addEventListener('click', e => {
+//         e.preventDefault()
+//         sectionContentElements[btnIndex].classList.toggle('hidden')
+//     })
+// })
+
+// // calcBtn.addEventListener('click', e => {
+// //     e.preventDefault()
+// //     let formData = new FormData(budgetForm)
+//     // {
+//     //     incomeMain: 2342.23,
+//     //     incomeOther: 21342.54
+//     // }
+// })
