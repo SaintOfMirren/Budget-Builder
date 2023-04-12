fa7d7a36ccab4a1b4fcafcbe3155b0869f3feaad
SaintOfMirren <drew.miller2015@gmail.com>
2023-04-11 20:23:36 -0700

    updated homeroutes.js to keep login cached on expense page and fixed route to go back home

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -43,15 +43,43 @@ router.get('/login', (req, res) => {
 });
 
 // Expense API Endpoint
-router.get('/expense', withAuth, async (req, res) => {
+router.get('/expense/:id', async (req, res) => {
   try {
     // // Find the logged in user based on the session ID but exclude the password field
-    res.render('expense');
-    const expenseData = await Expense.findAll({
-      where: {user_id: req.session.user_id}
+    const expenseData = await Expense.findByPk(req.params.id, {
+      include: [
+        {
+        model: User,
+        attributes: ['name'],
+        },
+      ],
     });
 
+    const expense = expenseData.get({ plain: true });
+    
     // Send Expense Data to Pie Chart
+    res.render('expense', {
+      ...expense,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/expense', withAuth, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Expense }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('expense', {
+      ...user,
+      logged_in: true
+    });
   } catch (err) {
     res.status(500).json(err);
   }
