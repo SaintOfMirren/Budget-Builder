f8c85b6c6622236e9c6372f1e2217fc79e87fe3f
Bootcamp <bootcamp@Abduls-MacBook-Air.local>
2023-04-12 13:54:17 -0700

    Expense Routes are working, chart is populating according to the expenses put in by the user, added a background to the page to make it look a bit modern

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,52 +1,55 @@
-const data = {
-  labels: ["Food", "Furniture", "Child Care"],
-  values: [500, 1000, 800],
-  colors: ["#FF6384", "#36A2EB", "#FFCE56"],
-  label: "Expenses",
-  hoverOffset: 4,
-  responsive: false,
-  maintainAspectRatio: false
-};
+fetch("/api/expense")
+  .then(res => res.json())
+  .then(expenses => {
+    console.log(expenses)
+    const categories = expenses.map(item => item.category)
+    const amounts = expenses.map(item => item.amount)
+    const data = {
+      labels: categories,
+      values: amounts,
+      colors: ["#FF6384", "#36A2EB", "#FFCE56"],
+      label: "Expenses",
+      hoverOffset: 4,
+      responsive: false,
+      maintainAspectRatio: false
+    };
 
-// const fetchData = async () => {
-//   const response = await fetch('/api/expense');
-//   const { labels, values } = await response.json();
-//   return { labels, values };
-// };
-
-const chartContainer = document.querySelector('#chart-container');
-const canvas = document.createElement('canvas');
-const chart = new Chart(canvas, {
-  type: 'pie',
-  data: {
-    labels: data.labels,
-    datasets: [{
-      data: data.values,
-      backgroundColor: data.colors,
-      hoverOffset: data.hoverOffset
-    }]
-  },
-  options: {
-    responsive: data.responsive,
-    maintainAspectRatio: data.maintainAspectRatio,
-    plugins: {
-      legend: {
-        position: 'bottom',
-        labels: {
-          font: {
-            size: 14
+    const chartContainer = document.querySelector('#chart-container');
+    const canvas = document.createElement('canvas');
+    const chart = new Chart(canvas, {
+      type: 'pie',
+      data: {
+        labels: data.labels,
+        datasets: [{
+          data: data.values,
+          backgroundColor: data.colors,
+          hoverOffset: data.hoverOffset
+        }]
+      },
+      options: {
+        responsive: data.responsive,
+        maintainAspectRatio: data.maintainAspectRatio,
+        plugins: {
+          legend: {
+            position: 'bottom',
+            labels: {
+              font: {
+                size: 14
+              }
+            }
+          },
+          title: {
+            display: true,
+            text: data.label,
+            font: {
+              size: 18
+            }
           }
         }
-      },
-      title: {
-        display: true,
-        text: data.label,
-        font: {
-          size: 18
-        }
       }
-    }
-  }
-});
+    });
 
-chartContainer.appendChild(canvas);
+    chartContainer.appendChild(canvas);
+  })
+
+
